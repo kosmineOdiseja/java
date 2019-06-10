9e5042af564240ec8af6345f1dc1271c36d3dfd9
Alius <aliusbosas@gmail.com>
2019-06-10 12:00:48 +0300

    pradinis komitas

---
+++ Mygtukas.java
@@ -0,0 +1,13 @@
+	import java.awt.Frame;
+	import java.applet.Applet;
+	import java.awt.Button;
+	
+	public class Mygtukas extends Frame, applet {
+          implements ActionListener, WindowListener, Mygtukas(){    
+		  
+		  		Button button1 = new Button();
+				button1.setLabel("vykdyti");
+				add(button1);
+    		
+    		}    
+     }
